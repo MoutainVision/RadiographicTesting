ac06a3ac0350ef9b5601e1a202b851423f8e4199
iczpchan <ykhmiou@hotmail.com>
2020-04-13 21:47:37 +0800

    實現查重功能

--- LibDefectRecognition/DCMIndexingFile.h
+++ LibDefectRecognition/DCMIndexingFile.h
@@ -26,6 +26,20 @@ struct LIBDEFECTRECOGNITION_API DCMFileIndex
 	unsigned nOffset;
 
 	unsigned nLength;
+
+	DCMFileIndex()
+		: strFullPath("")
+		, nOffset(0)
+		, nLength(0)
+	{
+	}
+
+	DCMFileIndex(string strPath, unsigned nOff, unsigned nLen)
+		: strFullPath(strPath)
+		, nOffset(nOff)
+		, nLength(nLen)
+	{
+	}
 };
 
 struct LIBDEFECTRECOGNITION_API DCMFileIndexingData
@@ -34,18 +48,17 @@ struct LIBDEFECTRECOGNITION_API DCMFileIndexingData
 
 	DCMFileFeat fileFeat;
 
-	unsigned nDefectNum;
-	vector<Defect> aDefectList;
+	vector<DefectFeat> aDefectList;
 };
 
 
-class DCMIndexingFile
+class LIBDEFECTRECOGNITION_API DCMIndexingFile
 {
 public:
 	DCMIndexingFile();
 	virtual ~DCMIndexingFile();
 
-	bool Create(const char *szNewFile)
+	static bool Create(const char *szNewFile="DCMIndexData.dat")
 	{
 		fstream fs;
 		fs.open(szNewFile, ios::out | ios::binary);
@@ -58,30 +71,7 @@ public:
 		return true;
 	}
 
-	bool Write(const char *szFile, const DCMFileIndexingData &data)
-	{
-		fstream fs;
-		fs.open(szFile, ios::app | ios::binary);
-		if (!fs)
-		{
-			return false;
-		}
-
-		unsigned nFilePathLength = data.strFullPath.size();
-		fs.write((char*)&nFilePathLength, sizeof(nFilePathLength));
-		fs.write(data.strFullPath.c_str(), data.strFullPath.size() * sizeof(char));
-
-		fs.write((char*)&data.fileFeat, sizeof(data.fileFeat));
-
-
-
-		fs.close();
-
-		return true;
-	}
-
-
-	bool GetFileSize(const char *szFileName, size_t &nFileSize)
+	static bool GetFileSize(const char *szFileName, size_t &nFileSize)
 	{
 		fstream f(szFileName, std::ios_base::binary | std::ios_base::in);
 		if (!f.good() || f.eof() || !f.is_open())
@@ -97,6 +87,84 @@ public:
 
 		nFileSize = f.tellg() - begin_pos;
 
+		return true;
+	}
+
+	static bool Write(DCMFileIndex &index, const char *szFile, const DCMFileIndexingData &data)
+	{
+		unsigned nSz;
+		if (!GetFileSize(szFile, nSz))
+		{
+			return false;
+		}
+
+		fstream fs;
+		fs.open(szFile, ios::app | ios::binary);
+		if (!fs)
+		{
+			return false;
+		}
+
+		unsigned nLength = 0;
+
+		unsigned nFilePathLength = data.strFullPath.size();
+		fs.write((char*)&nFilePathLength, sizeof(nFilePathLength));
+		nLength += sizeof(nFilePathLength);
+
+		fs.write(data.strFullPath.c_str(), data.strFullPath.size() * sizeof(char));
+		nLength += data.strFullPath.size() * sizeof(char);
+
+		fs.write((char*)&data.fileFeat, sizeof(data.fileFeat));
+		nLength += sizeof(data.fileFeat);
+
+		unsigned nDefect = data.aDefectList.size();
+		fs.write((char*)&nDefect, sizeof(nDefect));
+		nLength += sizeof(nDefect);
+
+		for (unsigned n = 0; n < nDefect; n++)
+		{
+			fs.write((char*)&data.aDefectList[n], sizeof(data.aDefectList[n]));
+			nLength += sizeof(data.aDefectList[n]);
+		}
+
+		fs.close();
+		
+		index.strFullPath = data.strFullPath;
+
+		index.nOffset = nSz;
+
+		index.nLength = nLength;
+
+		return true;
+	}
+
+	static bool Read(DCMFileIndexingData &data, const char *szFile, DCMFileIndex index)
+	{
+		fstream fs;
+		fs.open(szFile, ios::in | ios::binary);
+		if (!fs)
+		{
+			return false;
+		}
+
+		fs.seekg(index.nOffset, std::ios_base::beg);
+
+		unsigned nSz;
+		fs.read((char*)&nSz, sizeof(nSz));
+		fs.read((char*)&data.strFullPath, nSz);
+
+		fs.read((char*)&data.fileFeat, sizeof(data.fileFeat));
+
+		unsigned nDefect;
+		fs.read((char*)&nDefect, sizeof(nDefect));
+
+		for (unsigned n = 0; n < nDefect; n++)
+		{
+			fs.read((char*)&data.aDefectList[n], sizeof(data.aDefectList[n]));
+		}
+
+		fs.close();
+		
 		return true;
 	}
 
